3e50f7586f438c78b63b66b4ef54c061e94714a7
zouting <972721300@qq.com>
2017-12-20 10:11:06 +0800

    商品列表页表格样式微调

--- src/component/admin/goods/content/goodsContentList.vue
+++ src/component/admin/goods/content/goodsContentList.vue
@@ -5,7 +5,7 @@
             <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
             <el-breadcrumb-item>商品管理</el-breadcrumb-item>
             <el-breadcrumb-item>内容列表</el-breadcrumb-item>
-            <el-breadcrumb-item>评论详情</el-breadcrumb-item>
+            <el-breadcrumb-item>商品列表</el-breadcrumb-item>
         </el-breadcrumb>
         <!-- 工具条 -->
         <section class="list_util">
@@ -18,34 +18,66 @@
 
             <!-- 右边搜索框 -->
             <div class="list_util_search">
-                <el-input size="small" placeholder="请输入搜索内容" prefix-icon="el-icon-search">
+                <el-input size="small" placeholder="请输入搜索内容" prefix-icon="el-icon-search" @blur="getTableList">
                 </el-input>
             </div>            
         </section>
             <!-- 表格  表格: data属性为列表数据, 传入后会根据这个数据自动渲染  -->
-        <el-table ref="multipleTable" :data="dataList" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+        <el-table ref="multipleTable" :data="dataList" tooltip-effect="dark" style="width: 100%"    
+        height="400"  @selection-change="handleSelectionChange" >
             <!-- 多选框列 -->
             <el-table-column type="selection">
             </el-table-column>
+
             <!-- 普通列: label用于设置表头, prop用于指定该列展示的字段名称 -->
             <el-table-column label="标题">
-                <template slot-scope="scope">1111</template>
+                <template slot-scope="scope"></template>
             </el-table-column>
-             <!-- 普通列: label用于设置表头, 里面的template可以用于设定任意html结构 -->
-            <el-table-column prop="" label="所属类别">
-                <!-- 在temoplate里面, 需要通过scope.row拿到每一行数据 -->
+            
+            <el-table-column prop="categoryname" label="所属类别">                
+                <template slot-scope="scope"></template>
             </el-table-column>
+
+             <!-- 库存 -->
+            <el-table-column prop="stock_quantity" label="库存">
+                <template slot-scope="scope"></template>
+            </el-table-column>
+
+             <!-- 市场价 -->
+            <el-table-column prop="market_price" label="市场价">
+                <template slot-scope="scope"></template>
+            </el-table-column>
+             <!-- 销售价 -->
+            <el-table-column prop="sell_price" label="销售价">
+                <template slot-scope="scope"></template>
+            </el-table-column>
+             <!-- 属性 -->
+              <!-- 普通列: label用于设置表头, 里面的template可以用于设定任意html结构 -->
+              <!-- 在temoplate里面, 需要通过scope.row拿到每一行数据 -->
+            <el-table-column prop="status" label="属性">
+                <template slot-scope="scope">
+                    <i z="is_top"></i>
+                    <i z="is_hot"></i>
+                    <i z="is_slide"></i>
+                </template>
+            </el-table-column>
+             <!-- 操作 -->
+            <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <router-link :to="{name:'gcte',params:{id:scope.row.id}}">修改</router-link>
+                </template>
+                
+            </el-table-column>
+
         </el-table>
         <!-- 分页器 -->
+        <!-- 分页: size-change用于监听每页展示条目的变化, current-change用于监听页码的变化 -->
+        <!-- 分页: current-page属性用于设置当前处于第几页, page-size属性用于设置每页条目的可选项 -->
+        <!-- 分页: layout用于设置要使用那些分页小组件, total用于设置数据总量 -->
         <div class="block">
-            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage4" :page-sizes="[10, 20, 30, 40]" :page-size="10" layout="total, sizes, prev, pager, next, jumper" :total="40">
+            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="query.pageIndex" :page-sizes="[10, 20, 30, 40]" :page-size="query.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="40">
             </el-pagination>
         </div>
-
-    
-
-
-
     </div>
 </template>
 
@@ -53,18 +85,36 @@
     export default {
         data(){
             return {
-                dataList:[
-                    { date: 2017, name: '大花' },
-                    { date: 2016, name: '美美' },
-                    { date: 2015, name: '小花' },
-                ]
+                query:{
+                    pageIndex:1,
+                    pageSize:10,
+                    searchValue:''
+                },
+                totalcount:0,
+                dataList:[]
+               
             }
         },
         methods:{
-        handleSelectionChange(){},            
-
-        }
-
+            //1. 当焦点离开搜索框时, 调用接口获取搜索后的数据列表
+            getTableList(){
+                 // get方法的第二个参数可以用来指定查询字符串, header头信息等内容---用params将数据传过去
+                this.$http.get(this.$api.gsList,{params:this.query})
+                .then(rsp=>{
+                   this.totalcount=rsp.data.totalcount;
+                    this.dataList=rsp.data.message;
+                    
+                })
+            },
+            handleSelectionChange(){},
+            handleSizeChange(){},
+            handleCurrentChange(){}
+       
+        },
+    // 刚进入页面的时候应该请求数据
+            created(){
+                this.getTableList();
+            }
     }
 </script>
 
@@ -87,6 +137,9 @@
             &_search{
                 float: right;
             }
+        }
+        .block{
+            padding: 10px;
         }
     }
 </style>
